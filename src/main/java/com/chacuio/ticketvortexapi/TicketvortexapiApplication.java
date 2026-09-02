d02e787c3c2a937720d430fda88424ae701b1da2
Juan Pablo Bolanio <juampi360786@gmail.com>
2026-09-02 15:09:29 -0300

    chore(general): inicializar proyecto

---
+++ src/main/java/com/chacuio/ticketvortexapi/TicketvortexapiApplication.java
@@ -0,0 +1,13 @@
+package com.chacuio.ticketvortexapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketvortexapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketvortexapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chacuio/ticketvortexapi/TicketvortexapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chacuio.ticketvortexapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketvortexapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
