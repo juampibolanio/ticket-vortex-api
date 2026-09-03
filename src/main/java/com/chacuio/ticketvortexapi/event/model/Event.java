04cd6ac7736111241b0143776aa5205fae868784
Juan Pablo Bolanio <juampi360786@gmail.com>
2026-09-03 16:38:08 -0300

    feat(events): crear entidad event

---
+++ src/main/java/com/chacuio/ticketvortexapi/event/model/Event.java
@@ -0,0 +1,51 @@
+package com.chacuio.ticketvortexapi.event.model;
+
+import com.chacuio.ticketvortexapi.zone.model.Zone;
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+@Table(name = "events")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter @Setter
+@Builder
+@EntityListeners(AuditingEntityListener.class)
+public class Event {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false, length = 120)
+    private String title;
+
+    @Column(length = 500)
+    private String description;
+
+    @Column(nullable = false)
+    private LocalDateTime date;
+
+    @Column(nullable = false, length = 120)
+    private String location;
+
+    @CreatedDate
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private Instant createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at")
+    private Instant updatedAt;
+
+    @OneToMany(mappedBy = "event", fetch = FetchType.LAZY, cascade = CascadeType.ALL, orphanRemoval = true)
+    @Builder.Default
+    private List<Zone> zones = new ArrayList<>();
+}
