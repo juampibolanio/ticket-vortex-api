3079691a67e376d9d53be106288c8866cb66ce31
Juan Pablo Bolanio <juampi360786@gmail.com>
2026-09-03 14:53:07 -0300

    feat(users): agregar entidad User y enum Role

---
+++ src/main/java/com/chacuio/ticketvortexapi/user/model/Role.java
@@ -0,0 +1,6 @@
+package com.chacuio.ticketvortexapi.user.model;
+
+public enum Role {
+    CUSTOMER,
+    ADMIN
+}
---
+++ src/main/java/com/chacuio/ticketvortexapi/user/model/User.java
@@ -0,0 +1,54 @@
+package com.chacuio.ticketvortexapi.user.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.Instant;
+import java.util.UUID;
+
+@Entity
+@Table(name = "users")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter @Setter
+@Builder
+@EntityListeners(AuditingEntityListener.class)
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(name = "first_name", nullable = false, length = 100)
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false, length = 100)
+    private String lastName;
+
+    @Column(length = 255, unique = true, nullable = false)
+    private String email;
+
+    @Column(name = "document_number", nullable = false, unique = true, length = 10)
+    private String documentNumber;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @Column(name = "is_active", nullable = false)
+    @Builder.Default
+    private boolean isActive = true;
+
+    @CreatedDate
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private Instant createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at")
+    private Instant updatedAt;
+}
