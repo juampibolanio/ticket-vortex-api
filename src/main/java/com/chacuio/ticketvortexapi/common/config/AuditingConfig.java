606c93caf80bc6886a8abd3d7d94342773de9c8c
Juan Pablo Bolanio <juampi360786@gmail.com>
2026-09-04 15:23:13 -0300

    refactor(common): mover configuracion de auditoría jpa a /config

--- src/main/java/com/chacuio/ticketvortexapi/common/config/AuditingConfig.java
+++ src/main/java/com/chacuio/ticketvortexapi/common/config/AuditingConfig.java
@@ -1,4 +1,4 @@
-package com.chacuio.ticketvortexapi.common;
+package com.chacuio.ticketvortexapi.common.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
