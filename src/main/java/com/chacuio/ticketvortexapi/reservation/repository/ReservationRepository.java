4eec0285523a30c88f0d427f1536b70bdc58f901
Juan Pablo Bolanio <juampi360786@gmail.com>
2026-09-07 13:31:55 -0300

    feat(reservations): crear consulta personalizada para contar los lugares disponibles

--- src/main/java/com/chacuio/ticketvortexapi/reservation/repository/ReservationRepository.java
+++ src/main/java/com/chacuio/ticketvortexapi/reservation/repository/ReservationRepository.java
@@ -2,7 +2,18 @@ package com.chacuio.ticketvortexapi.reservation.repository;
 
 import com.chacuio.ticketvortexapi.reservation.model.Reservation;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.time.Instant;
 import java.util.UUID;
 
-public interface ReservationRepository extends JpaRepository<Reservation, UUID> { }
+public interface ReservationRepository extends JpaRepository<Reservation, UUID> {
+
+    @Query("SELECT COUNT(r) FROM Reservation r " +
+            "WHERE r.zone.id = :zone_id " +
+            "AND (r.status = :statusConfirmed OR (r.status = :statusReserved AND r.expiresAt > :current_time))")
+    Long countUnavailablePlacesByZone(
+            @Param("zone_id") UUID zoneId,
+            @Param("current_time") Instant currentTime);
+}
