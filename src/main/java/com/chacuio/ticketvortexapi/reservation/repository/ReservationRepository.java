d697af0d8cfa659e3221d79d7327bc060c7e0c03
Juan Pablo Bolanio <juampi360786@gmail.com>
2026-09-07 15:45:40 -0300

    feat(reservations): crear consulta personalizada para listar reservas

--- src/main/java/com/chacuio/ticketvortexapi/reservation/repository/ReservationRepository.java
+++ src/main/java/com/chacuio/ticketvortexapi/reservation/repository/ReservationRepository.java
@@ -1,11 +1,13 @@
 package com.chacuio.ticketvortexapi.reservation.repository;
 
+import com.chacuio.ticketvortexapi.reservation.dto.ReservationSummaryDTO;
 import com.chacuio.ticketvortexapi.reservation.model.Reservation;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.time.Instant;
+import java.util.List;
 import java.util.UUID;
 
 public interface ReservationRepository extends JpaRepository<Reservation, UUID> {
@@ -16,4 +18,23 @@ public interface ReservationRepository extends JpaRepository<Reservation, UUID> 
     Long countUnavailablePlacesByZone(
             @Param("zone_id") UUID zoneId,
             @Param("current_time") Instant currentTime);
+
+    @Query("""
+        SELECT new com.chacuio.ticketvortexapi.reservation.dto.ReservationSummaryDTO(
+            r.id,
+            u.id,
+            u.email,
+            z.id,
+            z.name,
+            r.status,
+            z.price,
+            r.expiresAt,
+            r.createdAt
+            )
+        FROM Reservation r
+        JOIN r.user u 
+        JOIN r.zone z
+        JOIN z.event e
+    """)
+    List<ReservationSummaryDTO> findAllSummarized();
 }
