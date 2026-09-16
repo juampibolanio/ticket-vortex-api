221d254eea8f7cfd42066176c4ed929c474dc630
Juan Pablo Bolanio <juampi360786@gmail.com>
2026-09-16 10:48:37 -0300

    feat(zones): crear mapper de zonas

---
+++ src/main/java/com/chacuio/ticketvortexapi/zone/mapper/ZoneMapper.java
@@ -0,0 +1,50 @@
+package com.chacuio.ticketvortexapi.zone.mapper;
+
+import com.chacuio.ticketvortexapi.event.dto.EventSummaryDTO;
+import com.chacuio.ticketvortexapi.event.mapper.EventMapper;
+import com.chacuio.ticketvortexapi.event.model.Event;
+import com.chacuio.ticketvortexapi.zone.dto.ZoneResponseDTO;
+import com.chacuio.ticketvortexapi.zone.dto.ZoneSummaryDTO;
+import com.chacuio.ticketvortexapi.zone.model.Zone;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class ZoneMapper {
+    private final EventMapper eventMapper;
+
+    public ZoneResponseDTO toDto(Zone zone) {
+        return new ZoneResponseDTO(
+                zone.getId(),
+                zone.getName(),
+                zone.getDescription(),
+                zone.getPrice(),
+                zone.getCapacity(),
+                eventMapper.toDtoSummary(zone.getEvent()),
+                zone.getVersion(),
+                zone.getCreatedAt(),
+                zone.getUpdatedAt()
+        );
+    }
+
+    public Zone toEntity(ZoneResponseDTO dto, Event event) {
+        return Zone.builder()
+                .name(dto.name())
+                .description(dto.description())
+                .price(dto.price())
+                .capacity(dto.capacity())
+                .event(event)
+                .build();
+    }
+
+    public ZoneSummaryDTO toDtoSummary(Zone zone) {
+        if (zone == null) return null;
+
+        return new ZoneSummaryDTO(
+                zone.getId(),
+                zone.getName(),
+                zone.getEvent().getId()
+        );
+    }
+}
