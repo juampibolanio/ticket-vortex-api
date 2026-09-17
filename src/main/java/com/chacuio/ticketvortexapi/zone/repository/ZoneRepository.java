97516b2cc0602ec77c5982276c717841f09fefb8
Juan Pablo Bolanio <juampi360786@gmail.com>
2026-09-17 10:20:46 -0300

    feat(zone): crear método para busqueda de zona por id con bloqueo

--- src/main/java/com/chacuio/ticketvortexapi/zone/repository/ZoneRepository.java
+++ src/main/java/com/chacuio/ticketvortexapi/zone/repository/ZoneRepository.java
@@ -1,8 +1,18 @@
 package com.chacuio.ticketvortexapi.zone.repository;
 
 import com.chacuio.ticketvortexapi.zone.model.Zone;
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.Optional;
 import java.util.UUID;
 
-public interface ZoneRepository extends JpaRepository<Zone, UUID> { }
+public interface ZoneRepository extends JpaRepository<Zone, UUID> {
+
+    @Lock(LockModeType.OPTIMISTIC_FORCE_INCREMENT)
+    @Query("SELECT z FROM Zone z WHERE z.id = :id")
+    Optional<Zone> findByIdWithLock(@Param("id") UUID id);
+}
