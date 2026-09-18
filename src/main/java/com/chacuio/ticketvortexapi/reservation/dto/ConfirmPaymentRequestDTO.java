d9635c402c3537e56c2b50f99ab544025af4739d
Juan Pablo Bolanio <juampi360786@gmail.com>
2026-09-18 15:27:19 -0300

    feat(reservations): agregar validaciones en dto´s

--- src/main/java/com/chacuio/ticketvortexapi/reservation/dto/ConfirmPaymentRequestDTO.java
+++ src/main/java/com/chacuio/ticketvortexapi/reservation/dto/ConfirmPaymentRequestDTO.java
@@ -1,8 +1,13 @@
 package com.chacuio.ticketvortexapi.reservation.dto;
 
+import jakarta.validation.constraints.NotBlank;
+
 import java.util.UUID;
 
 public record ConfirmPaymentRequestDTO(
+        @NotBlank(message = "Transaction Id is required")
         UUID idempotencyKey,
+
+        @NotBlank(message = "Transaction Id is required")
         UUID transactionId
 ) { }
--- src/main/java/com/chacuio/ticketvortexapi/reservation/dto/ReservationRequestDTO.java
+++ src/main/java/com/chacuio/ticketvortexapi/reservation/dto/ReservationRequestDTO.java
@@ -1,9 +1,21 @@
 package com.chacuio.ticketvortexapi.reservation.dto;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+
 import java.util.UUID;
 
 public record ReservationRequestDTO(
+        @NotBlank(message = "Zone Id is required")
         UUID zoneId,
+
+        @NotNull(message = "Quantity is required")
+        @Positive(message = "Quantity must be greater than zero")
+        @Min(1)
         Integer quantity,
+
+        @NotBlank(message = "Idempotency Key is required")
         UUID idempotencyKey
 ) {}
