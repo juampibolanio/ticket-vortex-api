5f957667781bd664d7f2a4aac2f290e878b24b44
Juan Pablo Bolanio <juampi360786@gmail.com>
2026-09-21 10:18:48 -0300

    feat(zones): agregar validaciones a DTOs de zonas

--- src/main/java/com/chacuio/ticketvortexapi/zone/dto/ZoneRequestDTO.java
+++ src/main/java/com/chacuio/ticketvortexapi/zone/dto/ZoneRequestDTO.java
@@ -1,12 +1,24 @@
 package com.chacuio.ticketvortexapi.zone.dto;
 
+import jakarta.validation.constraints.*;
+import org.hibernate.validator.constraints.Length;
+
 import java.math.BigDecimal;
-import java.util.UUID;
 
 public record ZoneRequestDTO(
+        @NotBlank(message = "Name is required")
+        @Length(min = 1, max = 120, message = "The name field must be between 1 and 120 characters long")
         String name,
+
+        @Length(max = 250, message = "The description must have a maximum of 250 characters")
         String description,
+
+        @NotNull(message = "Price is required")
+        @PositiveOrZero(message = "The price must be positive or zero")
         BigDecimal price,
-        Integer capacity,
-        UUID eventId
+
+        @NotNull(message = "Capacity is required")
+        @Positive(message = "The capacity must be positive")
+        @Min(1)
+        Integer capacity
 ) { }
