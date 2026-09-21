f7daf74538e139ecba4d069d2e797b1a7a21a8b0
Juan Pablo Bolanio <juampi360786@gmail.com>
2026-09-21 12:26:44 -0300

    feat(zones): crear consulta para buscar todas las zonas segun id del evento

--- src/main/java/com/chacuio/ticketvortexapi/zone/repository/ZoneRepository.java
+++ src/main/java/com/chacuio/ticketvortexapi/zone/repository/ZoneRepository.java
@@ -1,5 +1,7 @@
 package com.chacuio.ticketvortexapi.zone.repository;
 
+import com.chacuio.ticketvortexapi.zone.dto.ZoneResponseDTO;
+import com.chacuio.ticketvortexapi.zone.dto.ZoneSummaryDTO;
 import com.chacuio.ticketvortexapi.zone.model.Zone;
 import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -7,6 +9,7 @@ import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -15,4 +18,19 @@ public interface ZoneRepository extends JpaRepository<Zone, UUID> {
     @Lock(LockModeType.OPTIMISTIC_FORCE_INCREMENT)
     @Query("SELECT z FROM Zone z WHERE z.id = :id")
     Optional<Zone> findByIdWithLock(@Param("id") UUID id);
+
+    @Query("""
+            SELECT new com.chacuio.ticketvortexapi.zone.dto.ZoneSummaryDTO(
+                z.id,
+                z.name,
+                z.capacity,
+                z.event.id
+            )
+            FROM Zone z
+            WHERE z.event.id = :event_id
+        """)
+    List<ZoneSummaryDTO> findAllByEventId(@Param("event_id") UUID eventId);
+
+    @Query("SELECT z FROM Zone z JOIN FETCH z.event WHERE z.id = :zone_id")
+    Optional<Zone> findByIdWithEvent(@Param("zone_id") UUID zoneId);
 }
